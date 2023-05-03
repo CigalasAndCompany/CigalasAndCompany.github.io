9184bfaa79008ed43ee9a099549ed707814cef9d
CigalasAndCompany <131870335+CigalasAndCompany@users.noreply.github.com>
2023-05-03 09:17:03 +0200

    Delete subscribeform.php

--- subscribeform.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "cigalasandcompany@gmail.com"; // this is your Email address
-    $from = $_POST['_replyto']; // this is the sender's Email address
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = " Someone wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . "\n\n" . $_POST['message'];
-?>
